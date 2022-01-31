4e4a8f3d5fce021a0a352ac404e0faf525acb99c
김규빈 <9bin08@naver.com>
2022-01-31 22:19:36 +0900

    20220131

--- rateCount.js
+++ rateCount.js
@@ -4,5 +4,29 @@
 
 function solution(N, stages) {
   var answer = [];
-  return answer;
+
+  let stageLevel = stages.length;
+
+  for(let i = 1; i <= N; i++){
+    let failStage = 0;
+    let stageStep = stages.filter(el => el === i).length;
+    
+    if(stageStep ===0) {
+      failStage = 0;
+    }else {
+      failStage = stageStep / stageLevel
+    }
+
+    stageLevel -= stageStep;
+    answer.push({stage: i, result: failStage});
+  }
+
+  answer.sort((a,b) => {
+    if(a.result === b.result){
+      return a.stage - b.stage
+    } else{
+      return b.result - a.result
+    }
+  })
+  return answer.map(el => el.stage)
 }
