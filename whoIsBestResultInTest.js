81d1b110f19b4f6f8db46777f22c8dface700070
김규빈 <9bin08@naver.com>
2022-04-21 01:01:06 +0900

    20220421

--- whoIsBestResultInTest.js
+++ whoIsBestResultInTest.js
@@ -2,40 +2,21 @@
 // 모의고사 .
 //풀이
 
-
-//TODO : 또 나온 습관.. 쉽게 할 수 있을듯 ?
 function solution(answers) {
-  let answer = [];
-    const solutionPattern = [[1,2,3,4,5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] ]
-    let solutionResult = [[],[],[]]
-    let score = [{key:1,value:0},{key:2,value:0},{key:3,value:0}];
-    let key = []
-  for(let i in solutionPattern){
-    key[i] = Math.ceil(answers.length / solutionPattern[i].length)
-    }
+  var answer = [];
+  var a1 = [1, 2, 3, 4, 5];
+  var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
+  var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  for(let i in key){
+  var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
+  var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
+  var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
+  var max = Math.max(a1c,a2c,a3c);
 
-    for(let j=0;j<key[i];j++){
+  if (a1c === max) {answer.push(1)};
+  if (a2c === max) {answer.push(2)};
+  if (a3c === max) {answer.push(3)};
 
-      solutionResult[i].push(...solutionPattern[i])
-    }
-
-  }
   
-  for(let i in answers){
- 
-    if(solutionResult[0][i] == answers[i] ) score[0].value++
-    if(solutionResult[1][i] == answers[i] ) score[1].value++
-    if(solutionResult[2][i] == answers[i] ) score[2].value++
-  }
-  score.sort((a,b) => {
-    return b.value - a.value
-  })
-  for( let i of score){
-    if(i.value != 0)
-    answer.push(i.key)
-  }
-  
-return answer
+  return answer;
 }
